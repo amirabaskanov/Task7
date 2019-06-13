de239a6aca039eaa2c5252de02ee8da604f509cc
Amir A <absknv@gmail.com>
2019-06-13 12:00:18 +0600

    Task7

---
+++ task7.py
@@ -0,0 +1,20 @@
+def biggest_guy(x,y,z):
+    if x>=y and x>=z: return x
+    elif y>=x and y>=z: return y
+    else: return z
+def test_biggest_guy():
+  try:
+    assert biggest_guy(1, 3, 2) == 3
+    assert biggest_guy(30, 10, 20) == 30
+    assert biggest_guy(20, 10, 30) == 30
+    assert biggest_guy(2, 1, 9) == 9
+    assert biggest_guy('a', 'a', 'b') == 'b' # 'b' is greater than 'a'
+
+  except (AssertionError) as e:
+    print(e)
+    print("Wrong!!")
+    return
+
+  print("Correct buddy!!!")
+#test your code by un-commenting the line(s) below
+test_biggest_guy()
